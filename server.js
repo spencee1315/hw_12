155a57612b454874dfa26b4324e0f2b34462a1b2
Elliott Spencer <elliott.spencer1315@gmail.com>
2021-09-20 22:57:32 -0700

    function to show employees

--- server.js
+++ server.js
@@ -135,3 +135,25 @@ showRoles = () => {
       promptUser();
     })
   };
+
+  // function to show all employees 
+showEmployees = () => {
+    console.log('Showing all employees...\n'); 
+    const sql = `SELECT employee.id, 
+                        employee.first_name, 
+                        employee.last_name, 
+                        role.title, 
+                        department.name AS department,
+                        role.salary, 
+                        CONCAT (manager.first_name, " ", manager.last_name) AS manager
+                 FROM employee
+                        LEFT JOIN role ON employee.role_id = role.id
+                        LEFT JOIN department ON role.department_id = department.id
+                        LEFT JOIN employee manager ON employee.manager_id = manager.id`;
+  
+    connection.promise().query(sql, (err, rows) => {
+      if (err) throw err; 
+      console.table(rows);
+      promptUser();
+    });
+  };
