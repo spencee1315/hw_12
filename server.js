22e6e5bce75b8fc825a957418d68d7a34f6a39bf
Elliott Spencer <elliott.spencer1315@gmail.com>
2021-09-20 22:56:00 -0700

    added function for roles

--- server.js
+++ server.js
@@ -120,3 +120,18 @@ showDepartments = () => {
         promptUser();
     });
 };
+
+// function to show all roles 
+showRoles = () => {
+    console.log('Showing all roles...\n');
+  
+    const sql = `SELECT role.id, role.title, department.name AS department
+                 FROM role
+                 INNER JOIN department ON role.department_id = department.id`;
+    
+    connection.promise().query(sql, (err, rows) => {
+      if (err) throw err; 
+      console.table(rows); 
+      promptUser();
+    })
+  };
